0dda9cd5bb7d84eebc26df310aeb8616e4dac53a
dhruva_m <dhruvam.cs19@bmsce.ac.in>
2022-01-09 13:10:58 +0530

    project work

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:day35/pages/start.dart';
+import 'package:flutter/material.dart';
+// @dart=2.9
+
+void main () {
+  runApp(MaterialApp(
+    home: StartPage(),
+    debugShowCheckedModeBanner: false,
+  ));
+}
